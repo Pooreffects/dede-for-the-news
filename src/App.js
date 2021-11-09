1bd8d8e3bbc945f372f9658377061d81cace1893
Pooreffects <pooreffects@gmail.com>
2021-11-09 02:52:26 +0100

    chore: env and initial integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import env from 'react-dotenv';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: env.ALAN_SDK_KEY,
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          console.log(articles);
+        }
+      },
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Dede AI News App</h1>
+      <h1>Dede AI News Reporter</h1>
     </div>
   );
 };
